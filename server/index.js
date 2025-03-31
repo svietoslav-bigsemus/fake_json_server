4b4020a7e5687a08a924a81061cea8d3954b1e03
svietoslav-bigsemus <svietoslav.k@gmail.com>
2025-03-31 11:26:27 +0300

    Init json-server for deployment

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server')
+const path = require('path')
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, '../db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 3001
+server.listen(port, () => {
+    console.log(`🚀 JSON Server is running on port ${port}`)
+})
